5b466094925b636d2f4f708625907a96ac1f854d
ocean <47253870+see-ocean@users.noreply.github.com>
2020-03-28 18:45:51 +0100

    .

--- commands/global_clear.js
+++ commands/global_clear.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args, prefix, env) => {
   if(!((message.author.id=="494386855974928386")||(message.author.id=="438007845498257408"))) return;
   message.delete()
     env.split(`, `).forEach(c=>{
-      client.channels.cache.get(c).bulkDelete(args[1])
+      client.channels.cache.get(c).bulkDelete(args[0])
   })
-  message.channel.send(`${args[1]} Wiadomości poszły na spacer`)
+  message.channel.send(`${args[0]} Wiadomości poszły na spacer`)
 }
