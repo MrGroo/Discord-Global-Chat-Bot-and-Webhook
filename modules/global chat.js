f9c52b12bddb939d98e8c41739b9ca199fab18fe
ocean <47253870+see-ocean@users.noreply.github.com>
2020-04-06 16:54:54 +0200

    Update global chat.js

--- modules/global chat.js
+++ modules/global chat.js
@@ -4,7 +4,7 @@ exports.run = async (client, env) => {
   
     function webhooksend(msg, webhookClient, files) {
         if(files == ""){
-              webhookClient.send(`${msg.content.replace('<@494386855974928386>','<@ocean jest super więc go nie pinguje>')}`, {
+              webhookClient.send(`${msg.content.replace('<@494386855974928386>','<@ocean jest super więc go nie pinguje>').replace(`@ocean#5239`,`może morze jak ocean`)}`, {
                   username: `${msg.author.tag} (${msg.author.id})`,
                   avatarURL: `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}`,
                   tts: false
